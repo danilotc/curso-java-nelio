3a1474f1f18814a2215df3270288b31baa3a2566
Danilo Castro <danilotrindade87@gmail.com>
2022-03-20 18:03:26 -0300

    Escopo e inicialização de variáveis

---
+++ src/estruturacondicional/EscopoInicializacao.java
@@ -0,0 +1,29 @@
+package estruturacondicional;
+
+public class EscopoInicializacao {
+	
+	public static void main(String[] args) {
+		double preco = 150.00;
+		
+		/*
+		 * Esta variável não poderá ser utilizada
+		 * se não for inicializada, gerando erro
+		 * de compilação.
+		 */
+		double desconto;
+		
+		/*
+		 * A variável desconto será inicializada com
+		 * valor do produto multiplicado por 0.1 ou 
+		 * 0.05, evitando erro de compilação.
+		 */
+		if (preco < 250.00) {
+			desconto = preco * 0.1;
+		}
+		else {
+			desconto = 0;
+		}
+		
+		System.out.println(desconto);
+	}
+}
